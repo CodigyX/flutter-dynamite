4cf9a7746dc0376097cab0aceb372223e09447c2
bobyDeveloper <zS21004509@estudiantes.uv.mx>
2023-10-13 21:42:24 -0600

    Fixed flutter run on Android Device

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Berna\Desktop\flutter_windows_3.13.0-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Berna\tecnologias\flutter-dynamite"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\flutter-dynamite"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
