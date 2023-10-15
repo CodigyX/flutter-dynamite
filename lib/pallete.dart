744a85858bac544b3b8f6ec7a5f595af4f0fdde3
bobyDeveloper <zS21004509@estudiantes.uv.mx>
2023-10-14 23:47:42 -0600

    DevelopFixed

---
+++ lib/pallete.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const Color backgroundColor = Color.fromRGBO(24, 24, 32, 1);
+  static const Color gradient1 = Color(0xFF294D61);
+  static const Color gradient2 = Color(0xFF0F969C);
+  static const Color gradient3 = Color(0xFF6DA5C0);
+  static const Color borderColor = Color.fromRGBO(52, 51, 67, 1);
+  static const Color whiteColor = Colors.white;
+}
